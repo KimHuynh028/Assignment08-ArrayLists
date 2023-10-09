c0492cdba357d7f1b9ac9762230de2fc0e7c51d3
Preston Fawcett <136362421+mrfawcett@users.noreply.github.com>
2023-10-08 18:17:11 -0700

    Instructions completed

--- src/main/java/ReviewCollection.java
+++ src/main/java/ReviewCollection.java
@@ -2,6 +2,31 @@ package main.java;
 
 import java.util.ArrayList;
 
+/* INSTRUCTIONS
+ * You plan to analyze product reviews found on a website by looking for 
+ * keywords in posted reviews. The ProductReview class is used to represent a 
+ * single review. A product review consists of a product name and a review of 
+ * that product. The ReviewCollection class is used to represent a collection 
+ * of reviews to be analyzed.
+ * 
+ * The method addReview adds a single product review, represented by a 
+ * ProductReview object, to the ReviewCollection object. The addReview method 
+ * does the following when it adds a product review:
+ * - The ProductReview object is added to the reviewList instance variable.
+ * - The product name from the ProductReview object is added to the productList 
+ *   instance variable if the product name is not already found in productList.
+ * 
+ * 
+ * The method getNumGoodReviews returns the number of good reviews for a given 
+ * product name. A review is considered good if it contains the word "best". If 
+ * there are no reviews with a matching product name, the method returns 0.
+ * 
+ * Note: This check should not be case sensitive. That means if the review 
+ * contains "BEST" or "Best" the method should be considered good; however, if 
+ * the review contains a word like "asbestos" the review should NOT be considered 
+ * good.
+ */
+
 public class ReviewCollection {
     private ArrayList<ProductReview> reviewList;
     private ArrayList<String> productList;
